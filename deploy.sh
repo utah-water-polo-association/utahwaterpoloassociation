9c1564427de4f102a3fe50f27bcbd82d6d23c6f5
voidfiles <voidfiles@gmail.com>
2024-10-21 20:49:57 -0600

    rm INPUT_REMOTE_KEY_PASS

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 set -eu
 eval `ssh-agent -s`
-echo "$INPUT_REMOTE_KEY" | SSH_PASS="$INPUT_REMOTE_KEY_PASS" agent-add
+echo "$INPUT_REMOTE_KEY" | agent-add
 
 rsync -r --delete-after -e 'ssh -o StrictHostKeyChecking=no' output/* eczrvsmy@50.6.153.225:/home1/eczrvsmy/public_html/website_287658e3/
