0fb47bf8c4030ae367660484584dce8a2abc94b8
voidfiles <voidfiles@gmail.com>
2024-10-22 22:07:36 -0600

    Yo

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ echo "$INPUT_REMOTE_KEY"
 echo "$INPUT_REMOTE_KEY" | ssh-add -
 ssh-add -l
 ssh -v -o IgnoreUnknown=UseKeychain -o StrictHostKeyChecking=no -o AddKeysToAgent=yes -o UseKeychain=yes  eczrvsmy@50.6.153.225
-rsync -r --delete-after -e 'ssh -o StrictHostKeyChecking=No -o AddKeysToAgent=Yes -o UseKeychain=Yes' output/* eczrvsmy@50.6.153.225:/home1/eczrvsmy/public_html/website_287658e3/
+rsync -r --delete-after -e 'ssh -o IgnoreUnknown=UseKeychain -o StrictHostKeyChecking=No -o AddKeysToAgent=Yes -o UseKeychain=Yes' output/* eczrvsmy@50.6.153.225:/home1/eczrvsmy/public_html/website_287658e3/
