63408305ef3f9a61fcc6f859ef02dbe4b4a46d33
voidfiles <voidfiles@gmail.com>
2025-03-08 15:45:34 -0700

    Fix score reporting

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,4 @@ ssh-add -l
 ssh -v -o IgnoreUnknown=UseKeychain -o StrictHostKeyChecking=no -o AddKeysToAgent=yes -o UseKeychain=yes  eczrvsmy@50.6.153.225
 rsync -r --delete-after -e 'ssh -o IgnoreUnknown=UseKeychain -o StrictHostKeyChecking=No -o AddKeysToAgent=Yes -o UseKeychain=Yes' output/* eczrvsmy@50.6.153.225:/home1/eczrvsmy/public_html/website_287658e3/
 rsync -r --delete-after -e 'ssh -o IgnoreUnknown=UseKeychain -o StrictHostKeyChecking=No -o AddKeysToAgent=Yes -o UseKeychain=Yes' output/* eczrvsmy@50.6.153.225:/home1/eczrvsmy/public_html/
+ssh -v -o IgnoreUnknown=UseKeychain -o StrictHostKeyChecking=no -o AddKeysToAgent=yes -o UseKeychain=yes eczrvsmy@50.6.153.225 "chmod 755 /home1/eczrvsmy/public_html/uploads"
